4e7980ec983d246446f9e05a141f1cb9fa4eea03
ns <portification@gmail.com>
2018-10-06 22:12:52 +0200

    Add utility.ts with throws

--- src/utility.ts
+++ src/utility.ts
@@ -1,6 +1,33 @@
 import { assert, assertEquals } from "https://cdn.rawgit.com/qoh/assert/v0.1.1/src/index.ts";
 
-export function throws(test: () => any, constructor?: Function, message?: string): () => any {
+type Test = () => any;
+
+export function throws(test: Test): Test;
+export function throws(constructor: Function, test: Test): Test;
+export function throws(message: string, test: Test): Test;
+export function throws(constructor: Function, message: string, test: Test): Test;
+export function throws(a: Test | Function | string, b?: Test | string, c?: Test): () => any {
+	let constructor: Function | undefined;
+	let message: string | undefined;
+	let test: Test;
+
+	// TODO: Consider stricter runtime type-checking here
+	if (c !== undefined) {
+		constructor = a as Function;
+		message = b as string;
+		test = c;
+	} else if (b !== undefined) {
+		if (typeof a === "string") {
+			message = a as string;
+		} else {
+			constructor = a as Function;
+		}
+
+		test = b as Test;
+	} else {
+		test = a as Test;
+	}
+
 	return () => {
 		try {
 			test();
